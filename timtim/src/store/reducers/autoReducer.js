bd220fa037e654e66e958a40ec67d4b70a766c35
Tim <timba64@mail.ru>
2019-07-08 20:33:14 +0300

    change initState in autoReducer

--- timtim/src/store/reducers/autoReducer.js
+++ timtim/src/store/reducers/autoReducer.js
@@ -1,10 +1,14 @@
+// const initState = {
+//     autos: [
+//       { id: "1", title: "BMW-320", content: "1998y, 3.0, blue, super state, 5000$" },
+//       { id: "2", title: "Mercedes-s500", content: "2007y, 5.0, white, full options, leather interior, 4000$" },
+//       { id: "3", title: "Volkswagen Passat", content: "2008y, 2.0turbo, red, full options, super state, 6000$" }
+//     ]
+//   };
+
 const initState = {
-    autos: [
-      { id: "1", title: "BMW-320", content: "1998y, 3.0, blue, super state, 5000$" },
-      { id: "2", title: "Mercedes-s500", content: "2007y, 5.0, white, full options, leather interior, 4000$" },
-      { id: "3", title: "Volkswagen Passat", content: "2008y, 2.0turbo, red, full options, super state, 6000$" }
-    ]
-  };
+  autos: []
+}
 
   const autoReducer = (state = initState, action) => {
     switch (action.type) {
